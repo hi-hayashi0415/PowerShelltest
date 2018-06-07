83193e589d14457b119daf93d35054de3e849d92
HayashiHironori <hi_hayashi@reglecasse.co.jp>
2018-06-07 14:29:17 +0900

    add file Get-PCRole

---
+++ TEST/Get-PCRole.ps1
@@ -0,0 +1,14 @@
+﻿#Win32_ComputerSystemオブジェクトを取得して変数に格納
+$cs = Get-WmiObject -Class Win32_ComputerSystem
+
+#コンピューターの役割を表すDomainRoleプロパティを変数に格納
+$DomainRole = $cs.DomainRole
+
+#DomainRoleプロパティの値に応じて処理を振り分ける
+Switch -regex ($DomainRole)
+{
+  [0-1]{"このコンピューターは、ワークステーションです。"}
+  [2-3]{"このコンピューターはサーバーですが、ドメインコントローラーではありません"}
+  [4-5]{"このコンピューターは、ドメインコントローラーです。"}
+  default{"不明な値です"}
+}
