efe61ed9a26899df51b6681201152ea1a161324b
HayashiHironori <hi_hayashi@reglecasse.co.jp>
2018-06-08 15:53:51 +0900

    add shell file

---
+++ TEST/SuspendServices.ps1
@@ -0,0 +1,11 @@
+﻿# サービスの一覧を取得
+$services = Get-Service
+foreach($svc in $services)
+{
+  # サービスが一時停止でかつ現在実行中の場合
+  if(($svc.CanPauseAndContinue) -AND ($svc.Status -eq "Running"))
+  {
+    # サービスを一時停止するかどうかを確認する
+    Suspend-Service $svc.Name -Confirm
+  }
+}
---
+++ TEST/showServices.ps1
@@ -0,0 +1,33 @@
+﻿# 関数の定義
+# 引数としてサービスを受け取り
+# 停止状態のサービスは赤で表示する関数
+# 呼び出す関数は先に定義しておく必要がある
+function showStatus
+{
+  if($args[0].Status -eq "Running")
+  {
+   # サービス名を表示し、パディングする
+   Write-Host ($args[0].ServiceName.PadRight(30) +
+   $args[0].Status) -ForegroundColor Yellow
+  }
+  else
+  {
+   Write-Host ($args[0].ServiceName.PadRight(30) +
+   $args[0].Status) -ForegroundColor Red
+  }
+  return
+}
+
+# スクリプトはここから開始
+# サービスのリストを変数に格納
+$services = Get-Service
+
+# 各サービスの状態を表示
+foreach($svc in $services)
+{
+  showStatus $svc
+}
+
+
+
+
