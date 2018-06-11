8b7cffc897ae8a7f91c7dd94c20815df0c82840f
HayashiHironori <hi_hayashi@reglecasse.co.jp>
2018-06-11 15:53:24 +0900

    Ver Usual

---
+++ TEST/SelectErrorAndSeriousFromAllEventLogVerUsual.ps1
@@ -0,0 +1,12 @@
+﻿$LogNames=Get-WinEvent -ListLog * | `
+  Where-Object{($_.RecordCount -ne 0) -and ($_.IsEnabled -eq 1)} | `
+  ForEach-Object{$_.LogName}
+  ForEach ( $LogName in $LogNames)
+{
+  Get-WinEvent -LogName $LogName | `
+  # AddHours(-1)が過去1時間を指定
+  Where-Object{($_.TimeCreated -ge (get-date).AddHours(-1)) -and `
+  # 「重大」と「エラー」を指定
+                (($_.Level -eq 1) -or ($_.Level -eq 2))} | `
+  Format-Table LogName, LevelDisplayName, TimeCreated, Id, Message
+}
