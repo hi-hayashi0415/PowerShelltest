051f9938f037812cbfb759ce2f044e5971f0c34b
HayashiHironori <hi_hayashi@reglecasse.co.jp>
2018-06-11 15:51:07 +0900

    add AllDeleteEventLogEntry

---
+++ TEST/AllDeleteEventLogEntryFromAllEventLog.ps1
@@ -0,0 +1,15 @@
+﻿# 全てのイベントログからイベントエントリを削除するスクリプト
+
+$LogNames=Get-WinEvent -ListLog * | `
+  Where-Object{ ($_.RecordCount -ne 0) -and ($_.IsEnabled -eq 1)} | `
+  ForEach-Object{$_.LogName}
+  ForEach( $LogName in $LogNames)
+  {
+# WevtUtilでイベントログを削除する。
+    WevtUtil Clear-Log $LogName
+# 削除した旨を表示
+    Write-host $LogName "ログからイベントエントリを削除しました"
+　}
+# システムログには9行目でイベントエントリを削除した旨が新たなイベントエントリとして書き込まれるためそれを削除
+WevtUtil Clear-Log System
+
